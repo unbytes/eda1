67af105dd9b5006630970eec66dcfb2170a4bae3
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-12 15:24:39 -0300

    fix(libs): rm unnecessary pair of binary search

--- libs/binary_search.h
+++ libs/binary_search.h
@@ -1,19 +1,13 @@
-#include "pair_int.h"
-
-ii binary_search(int* arr, int n, int t) {
+int binary_search(int* arr, int n, int t) {
     int l = 0, r = n;
-    ii ans = { false, -1 };
 
     while (l <= r) {
         int m = (l + r) / 2;
 
-        if (arr[m] == t) {
-            ii ans = { true, m };
-            return ans;
-        }
+        if (arr[m] == t) return m;
         else if (arr[m] < t) l = m + 1;
         else r = m - 1;
     }
 
-    return ans;
+    return -1;
 }
