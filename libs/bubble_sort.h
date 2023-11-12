a4b467641b8a239cecc5a2e04b84ef2eafd1101a
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-12 01:34:32 -0300

    libs(sorting): bubble sort iter

---
+++ libs/bubble_sort.h
@@ -0,0 +1,9 @@
+#include "swap.h"
+
+void bubble_sort(int* arr, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j < n - i; j++) {
+            if (arr[j - 1] > arr[j]) swap(&arr[j - 1], &arr[j]);
+        }
+    }
+}
