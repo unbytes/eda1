f5d6b107a2d4f58c42ea389b5edb00b273db37a1
MateusVrs <mateusvrs.dev@gmail.com>
2023-11-14 11:10:36 -0300

    libs(sorting): quick sort algorithm

---
+++ libs/quick_sort.h
@@ -0,0 +1,24 @@
+#ifndef SWAP_LIB
+#define SWAP_LIB
+#include "swap.h"
+#endif
+
+int partition(int* arr, int l, int r) {
+    int pivot = arr[r], j = l;
+
+    for (int i = l; i < r; i++) {
+        if (arr[i] < pivot) swap(&arr[i], &arr[j++]);
+        else if (arr[j] < pivot) j = i;
+    }
+    swap(&arr[j], &arr[r]);
+
+    return j;
+}
+
+void quick_sort(int* arr, int l, int r) {
+    if (l > r) return;
+
+    int p = partition(arr, l, r);
+    quick_sort(arr, l, p - 1);
+    quick_sort(arr, p + 1, r);
+}
