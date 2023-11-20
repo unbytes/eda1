63eda5744021ae425e470808021756b1485899d1
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-20 17:24:13 -0300

    libs(sorting): add range sort in shell function

--- libs/shell_sort.h
+++ libs/shell_sort.h
@@ -5,11 +5,11 @@
 
 int shell_sort(int* arr, int l, int r) {
     int h = 1;
-    while (h < r / 3) h = 3 * h + 1;
+    while (h < (r - l) / 3) h = 3 * h + 1;
 
     for (; h > 0; h /= 3) {
-        for (int i = h; i < r; i++) {
-            for (int j = i; j >= h && arr[j] < arr[j - h]; j -= h)
+        for (int i = l + h; i < r; i++) {
+            for (int j = i; j >= l + h && arr[j] < arr[j - h]; j -= h)
                 swap(&arr[j], &arr[j - h]);
         }
     }
