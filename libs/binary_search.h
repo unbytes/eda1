0160a38cad86d0151692c20a231858b1a40c2496
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-20 16:10:56 -0300

    libs(search): non-overflow average in bin search

--- libs/binary_search.h
+++ libs/binary_search.h
@@ -2,7 +2,7 @@ int binary_search(int* arr, int n, int t) {
     int l = 0, r = n;
 
     while (l <= r) {
-        int m = (l + r) / 2;
+        int m = l + (r - l) / 2;
 
         if (arr[m] == t) return m;
         else if (arr[m] < t) l = m + 1;
